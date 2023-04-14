0648061b524a7060f7bee3d7527a361d50f65d74
HoniTahina <ricatahina@gmail.com>
2023-04-14 20:46:32 +0000

    image

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:tp2/page/signup.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,7 +10,7 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget{
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
@@ -17,46 +18,43 @@ class MyApp extends StatefulWidget{
 }
 
 class _MyAppState extends State<MyApp> {
-
-
   firebase_auth.FirebaseAuth firebaseAuth = firebase_auth.FirebaseAuth.instance;
 
-  void connexion() async{
-
-    try{
+  void connexion() async {
+    try {
       await firebaseAuth.createUserWithEmailAndPassword(
-        email: 'mamadev@herbidev.com', password: 'password123');
+          email: 'mamadev@herbidev.com', password: 'password123');
 
-        await firebaseAuth.createUserWithEmailAndPassword(
-        email: 'herbilot@herbidev.com', password: 'password123');
+      await firebaseAuth.createUserWithEmailAndPassword(
+          email: 'herbilot@herbidev.com', password: 'password123');
 
-        await firebaseAuth.createUserWithEmailAndPassword(
-        email: 'papadev@herbidev.com', password: 'password123');
-
-    }on FirebaseException catch (e){
+      await firebaseAuth.createUserWithEmailAndPassword(
+          email: 'papadev@herbidev.com', password: 'password123');
+    } on FirebaseException catch (e) {
       print(e);
     }
   }
 
-
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: 
-        Text('ceci est un tire'),
-        ),
-        body:Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('We are the dream team'),
-              ElevatedButton(onPressed: connexion, child: 
-              Text('Rjoignez-nous'))
-          ],),
-        ) 
-        
-      ),
+      home: Signup(),
+
+      // home: Scaffold(
+      //   appBar: AppBar(title:
+      //   Text('ceci est un tire'),
+      //   ),
+      //   body:Center(
+      //     child: Column(
+      //       mainAxisAlignment: MainAxisAlignment.center,
+      //       children: [
+      //         Text('We are the dream team'),
+      //         ElevatedButton(onPressed: connexion, child:
+      //         Text('Rjoignez-nous'))
+      //     ],),
+      //   )
+
+      // ),
     );
   }
 }
